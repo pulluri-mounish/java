92b91e9c13e2d8b8bdf73756a2eaea101e9f5ccb
pulluri-mounish <151706266+pulluri-mounish@users.noreply.github.com>
2024-08-17 11:34:53 +0530

    Create Addition.java

---
+++ Addition.java
@@ -0,0 +1,9 @@
+import java.io.*;
+class Supraction{
+    public static void main(String[] args[])
+      int a=5;
+      int b=10;
+      int c=a+b;
+System.out.println("Sum of" +a+ "and" +b+ "is" +c);
+  }
+}
