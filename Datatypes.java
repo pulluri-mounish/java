9dbd7c7fde8e4757ee7d69999700dc73e8ce7656
pulluri-mounish <151706266+pulluri-mounish@users.noreply.github.com>
2024-09-12 12:19:39 +0530

    Create Datatypes.java

---
+++ Datatypes.java
@@ -0,0 +1,15 @@
+import java.io.*;
+class Datatypes1{
+	Static int a;
+	Static float b;
+	Static double c;
+	Static boolean d;
+	Static long l;
+	public static void main(String agrs[]){
+		System.out.println("Default value of int is " +a);
+		System.out.println("Default value of float is " +b);
+		System.out.println("Default value of double is " +c);
+		System.out.println("Default value of boolean is " +d);
+		System.out.println("Default value of long is " +l);
+	}
+}
