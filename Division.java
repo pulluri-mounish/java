3fc6a5b208b06534f83ea56ef29856cccbcac9b7
pulluri-mounish <151706266+pulluri-mounish@users.noreply.github.com>
2024-09-12 12:20:37 +0530

    Create Division.java

---
+++ Division.java
@@ -0,0 +1,9 @@
+import java.io.*;
+class Division{
+   public static void main(String[] args){
+      int a=5;
+      int b=10;
+      int c=a/b;
+      System.out.println("Division of " +a+ " and " +b+ " is " +c);
+   }
+}
