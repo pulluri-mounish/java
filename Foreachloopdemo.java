4dcb3beb1cf6232c474d01925e3cfcb9a6a3d3b7
pulluri-mounish <151706266+pulluri-mounish@users.noreply.github.com>
2024-09-12 12:27:39 +0530

    Create Foreachloopdemo.java

---
+++ Foreachloopdemo.java
@@ -0,0 +1,9 @@
+import java.io.*;
+public class Foreachloopdemo{
+    public static void main(String args[]){
+        int arr[] = {10, 20, 30, 40, 50};
+        for(int i=0; i<5; i++){
+            System.out.println("The array elements are", arr[i]);
+        }
+    }
+}
